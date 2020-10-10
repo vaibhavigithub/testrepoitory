f5d0f873a8ed1affc07483d47040e816b0326536
unknown <vaibhavireddy.96@gmail.com>
2020-10-10 13:07:45 +0530

    armstrn

---
+++ Armstrong.java
@@ -0,0 +1,20 @@
+class Armstrong 
+{
+	public static void main(String[] args) 
+	{
+		int n=371;
+		int temp=n;
+	     int sum=0;
+		 while(n!=0)
+		{
+			 int rem=n%10;
+			 sum=sum+(rem*rem*rem);
+			 n=n/10;
+		}
+			 if(temp==sum)
+
+		System.out.println("armstrong");
+		else
+			System.out.println("not");
+	}
+}
